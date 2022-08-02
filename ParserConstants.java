29fc049bf0e1edd1750fd5ea75abccbe22421d24
amaral01 <amaral7999@gmail.com>
2022-08-02 19:04:34 -0300

    Initial commit

---
+++ ParserConstants.java
@@ -0,0 +1,64 @@
+public interface ParserConstants
+{
+    int FIRST_SEMANTIC_ACTION = 14;
+
+    int SHIFT  = 0;
+    int REDUCE = 1;
+    int ACTION = 2;
+    int ACCEPT = 3;
+    int GO_TO  = 4;
+    int ERROR  = 5;
+
+    int[][][] PARSER_TABLE =
+    {
+        { {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {SHIFT ,  2}, {ERROR ,  0}, {GO_TO ,  1}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} },
+        { {ACCEPT,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} },
+        { {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {SHIFT ,  3}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} },
+        { {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {SHIFT ,  5}, {ERROR ,  0}, {ERROR ,  0}, {SHIFT ,  6}, {SHIFT ,  7}, {ERROR ,  0}, {GO_TO ,  4}, {ERROR ,  0}, {ERROR ,  0} },
+        { {REDUCE,  0}, {SHIFT ,  9}, {SHIFT , 10}, {SHIFT , 11}, {SHIFT , 12}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {GO_TO ,  8}, {ERROR ,  0} },
+        { {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {SHIFT ,  5}, {ERROR ,  0}, {ERROR ,  0}, {SHIFT ,  6}, {SHIFT ,  7}, {ERROR ,  0}, {GO_TO , 13}, {ERROR ,  0}, {ERROR ,  0} },
+        { {REDUCE,  3}, {REDUCE,  3}, {REDUCE,  3}, {REDUCE,  3}, {REDUCE,  3}, {ERROR ,  0}, {REDUCE,  3}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} },
+        { {REDUCE,  4}, {REDUCE,  4}, {REDUCE,  4}, {REDUCE,  4}, {REDUCE,  4}, {ERROR ,  0}, {REDUCE,  4}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} },
+        { {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {SHIFT ,  5}, {ERROR ,  0}, {ERROR ,  0}, {SHIFT ,  6}, {SHIFT ,  7}, {ERROR ,  0}, {GO_TO , 14}, {ERROR ,  0}, {ERROR ,  0} },
+        { {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {REDUCE,  5}, {ERROR ,  0}, {ERROR ,  0}, {REDUCE,  5}, {REDUCE,  5}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} },
+        { {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {REDUCE,  6}, {ERROR ,  0}, {ERROR ,  0}, {REDUCE,  6}, {REDUCE,  6}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} },
+        { {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {REDUCE,  7}, {ERROR ,  0}, {ERROR ,  0}, {REDUCE,  7}, {REDUCE,  7}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} },
+        { {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {REDUCE,  8}, {ERROR ,  0}, {ERROR ,  0}, {REDUCE,  8}, {REDUCE,  8}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} },
+        { {ERROR ,  0}, {SHIFT ,  9}, {SHIFT , 10}, {SHIFT , 11}, {SHIFT , 12}, {ERROR ,  0}, {SHIFT , 15}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {GO_TO ,  8}, {ERROR ,  0} },
+        { {REDUCE,  1}, {REDUCE,  1}, {REDUCE,  1}, {REDUCE,  1}, {REDUCE,  1}, {ERROR ,  0}, {REDUCE,  1}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {GO_TO ,  8}, {ERROR ,  0} },
+        { {REDUCE,  2}, {REDUCE,  2}, {REDUCE,  2}, {REDUCE,  2}, {REDUCE,  2}, {ERROR ,  0}, {REDUCE,  2}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0}, {ERROR ,  0} }
+    };
+
+    int[][] PRODUCTIONS =
+    {
+        { 11, 3 },
+        { 12, 3 },
+        { 12, 3 },
+        { 12, 1 },
+        { 12, 1 },
+        { 13, 1 },
+        { 13, 1 },
+        { 13, 1 },
+        { 13, 1 }
+    };
+
+    String[] PARSER_ERROR =
+    {
+        "Erro estado 0",
+        "Erro estado 1",
+        "Erro estado 2",
+        "Erro estado 3",
+        "Erro estado 4",
+        "Erro estado 5",
+        "Erro estado 6",
+        "Erro estado 7",
+        "Erro estado 8",
+        "Erro estado 9",
+        "Erro estado 10",
+        "Erro estado 11",
+        "Erro estado 12",
+        "Erro estado 13",
+        "Erro estado 14",
+        "Erro estado 15"
+    };
+}
